4c52fb9014bbc36d336a06c3d241f6e850abc1e6
XXVIIHR <hakimzade.27@gmail.com>
2022-12-28 02:55:18 +0400

    Moderations

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -7,11 +7,13 @@ const Logo = props => {
     return (
         <Link to="/">
             <div className="Logo" {...props}>
-                {/* <img src="./logo/logo.png" /> */}
-                <span>Khan Store</span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFuSgTeplM86DeyH9OTEtsaDEjbpouPDJI6w_6Gh_MetMH5159PQK2NdFAOnjL7F9Vm1I&usqp=CAU" />
+                <div class = "logoname">
+                    <span>myPlay</span>
+                </div>
             </div>
         </Link>
-        
+
     );
 }
 
